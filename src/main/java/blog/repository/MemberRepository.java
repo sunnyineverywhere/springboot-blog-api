61afd430e88729c6b1f7a785e08a261892ced0eb
sunnyineverywhere <sunnyinsummer@ewhain.net>
2023-05-04 12:33:13 +0900

    ✨ feat: 레포지토리 인터페이스 생성

---
+++ src/main/java/blog/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package blog.repository;
+
+import blog.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
---
+++ src/main/java/blog/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package blog.repository;
+
+import blog.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
